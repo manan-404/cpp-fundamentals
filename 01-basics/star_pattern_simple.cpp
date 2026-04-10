a351e342044ccd3163d82d2fe1d0249c944fc123
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-10 22:53:41 +0500

    Added simple star pattern program using cout
    This program prints a simple triangle star pattern using manual formatting.

---
+++ 01-basics/star_pattern_simple.cpp
@@ -0,0 +1,16 @@
+// Topic: Basics
+// Program: Simple star pattern using cout
+// Description: Prints a triangle pattern using manual formatting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*\n";
+    cout << "*\t*\n";
+    cout << "*\t*\t*\n";
+    cout << "*\t*\t*\t*";
+
+    return 0;
+}
