7d590d2f0819eef793a664deee424416fc3b8f72
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-10 22:52:32 +0500

    Added formatted output program using escape sequences

---
+++ 01-basics/formated_output.cpp
@@ -0,0 +1,14 @@
+// Topic: Basics
+// Program: Formatted Output using escape sequences
+// Description: Prints characters with tabs and newline formatting
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "'s'\t'u'\t'k'\t'k'\t'u'\t'r'\n";
+    cout << "IBA University";
+
+    return 0;
+}
