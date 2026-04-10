639035f2006812689ce27e9266cb3a4883368f08
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-10 20:25:34 +0500

    Added Hello World program (Basics)
    First C++ program demonstrating basic output using cout.

---
+++ basics/hello_world.cpp
@@ -0,0 +1,11 @@
+// Adding first code of C++
+// Printing Hello world
+#include <iostream>
+using namespace
+
+int main(){
+    cout << "Hello world";
+    return 0;
+}
+
+// Added Hello World program
