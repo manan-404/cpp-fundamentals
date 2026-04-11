1c7c3ed6ba93a5558cadd1ec67f0538dd5491179
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-12 00:47:29 +0500

    Add comments example in comments.cpp
    Added comments to demonstrate usage of single-line and multi-line comments in C++.

---
+++ 01-basics/comments.cpp
@@ -0,0 +1,11 @@
+// learing comments
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"He is a good and kind person. He has attractive personality"<<endl;
+// cout<<"even he is a alone, He is .............\n";
+/*cout<<"he is bad person, he is brutel.";
+cout<<"also he posses boring personality";  */
+return 0;
+}
