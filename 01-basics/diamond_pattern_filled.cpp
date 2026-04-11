5c14f9de9e30025fa63b6a7a49de1c69c932cdbe
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-12 00:49:07 +0500

    Added filled diamond pattern program
    This program displays a filled diamond pattern using stars.

---
+++ 01-basics/diamond_pattern_filled.cpp
@@ -0,0 +1,25 @@
+// Topic: Basics
+// Program: Filled Diamond Pattern
+// Description: Displays a diamond shape using stars
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\t\t       *\n";
+    cout << "\t\t      ***\n";
+    cout << "\t\t     *****\n";
+    cout << "\t\t    *******\n";
+    cout << "\t\t   *********\n";
+    cout << "\t\t  ***********\n";
+    cout << "\t\t *************\n";
+    cout << "\t\t  ***********\n";
+    cout << "\t\t   *********\n";
+    cout << "\t\t    *******\n";
+    cout << "\t\t     *****\n";
+    cout << "\t\t      ***\n";
+    cout << "\t\t       *\n";
+
+    return 0;
+}
