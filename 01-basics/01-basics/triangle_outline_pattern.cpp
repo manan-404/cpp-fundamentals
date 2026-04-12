5e764a3072321b5b0f8f9c7391139e0d32f7b8eb
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-12 22:50:44 +0500

    Added triangle outline pattern program
    Displays a hollow triangle pattern using stars.

---
+++ 01-basics/01-basics/triangle_outline_pattern.cpp
@@ -0,0 +1,20 @@
+// Topic: Basics
+// Program: Triangle Outline Pattern
+// Description: Displays a hollow triangle using stars
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\t       *\n";
+    cout << "\t      * *\n";
+    cout << "\t     *   *\n";
+    cout << "\t    *     *\n";
+    cout << "\t   *       *\n";
+    cout << "\t  *         *\n";
+    cout << "\t *           *\n";
+    cout << "\t* * * * * * * *\n";
+
+    return 0;
+}
