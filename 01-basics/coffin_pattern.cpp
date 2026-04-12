92780b8eae762b521fbb456f7385455f7f7b3bb8
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-12 22:48:45 +0500

    Added coffin shape pattern program
    Displays a simple coffin shape using stars.

---
+++ 01-basics/coffin_pattern.cpp
@@ -0,0 +1,20 @@
+// Topic: Basics
+// Program: Coffin Shape Pattern
+// Description: Displays a simple coffin shape using stars
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\t     ***\n";
+    cout << "\t   *     *\n";
+    cout << "\t  *       *\n";
+    cout << "\t *         *\n";
+    cout << "\t  *       *\n";
+    cout << "\t   *     *\n";
+    cout << "\t    *   *\n";
+    cout << "\t     ***\n";
+
+    return 0;
+}
