d7ea1b56cfd8b2752130e9d11738390e3bb2aa57
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-12 22:47:11 +0500

    Added name box pattern using stars
    This program displays a name inside a star box format.

---
+++ 01-basics/name_box_pattern.cpp
@@ -0,0 +1,15 @@
+// Topic: Basics
+// Program: Name Box Pattern
+// Description: Displays a name inside a star box
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*****************\n";
+    cout << "*\tMANAN\t*\n";
+    cout << "*****************\n";
+
+    return 0;
+}
