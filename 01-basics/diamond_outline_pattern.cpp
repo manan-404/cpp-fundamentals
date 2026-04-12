a7695072e9ff9b9a1d3416cce328265d245bfb0f
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-12 22:49:53 +0500

    Added diamond outline pattern program
    Displays a hollow diamond shape using stars.

---
+++ 01-basics/diamond_outline_pattern.cpp
@@ -0,0 +1,21 @@
+// Topic: Basics
+// Program: Diamond Outline Pattern
+// Description: Displays a hollow diamond shape using stars
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\t        *\n";
+    cout << "\t      *   *\n";
+    cout << "\t    *       *\n";
+    cout << "\t  *           *\n";
+    cout << "\t*               *\n";
+    cout << "\t  *           *\n";
+    cout << "\t    *       *\n";
+    cout << "\t      *   *\n";
+    cout << "\t        *\n";
+
+    return 0;
+}
