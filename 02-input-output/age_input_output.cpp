0ddad84ae0cb3f8a6d6d615cc8128102c3a41df7
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-13 10:17:25 +0500

    Added age input/output program
    This program prompts the user to enter their age and then displays it.

---
+++ 02-input-output/age_input_output.cpp
@@ -0,0 +1,19 @@
+// Lab: 2
+// Topic: Input/Output
+// Program: Age Input Output
+// Description: Takes age as input and displays it
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Your age is " << age << endl;
+
+    return 0;
+}
