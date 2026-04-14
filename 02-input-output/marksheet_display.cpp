6b80bc3aa0d58ac390b9caf7892a30d5b4885804
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-14 22:11:15 +0500

    Added formatted marksheet display program
    Displays a formatted marksheet with subject scores.

---
+++ 02-input-output/marksheet_display.cpp
@@ -0,0 +1,20 @@
+// Lab: 2
+// Topic: Output Formatting
+// Program: Marksheet Display
+// Description: Displays a formatted marksheet
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "=====================================\n";
+    cout << "\t\tMARKSHEET\n";
+    cout << "=====================================\n";
+    cout << "ICT\t\t: 76\n";
+    cout << "English\t\t: 88\n";
+    cout << "Math\t\t: 70\n";
+    cout << "Total\t\t: 234 / 300\n";
+
+    return 0;
+}
