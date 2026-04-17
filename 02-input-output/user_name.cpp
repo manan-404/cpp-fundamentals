f2443f9329ee06664cb2648867fdc90f76064ffe
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-17 22:42:46 +0500

    Implement user input for name and output it
    This program asks full name of user using string data type and then prints user name.

---
+++ 02-input-output/user_name.cpp
@@ -0,0 +1,17 @@
+// using string to take input from user
+//  asking name from user as input
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string name;
+	cout<<"Enter your complete name ";
+	getline (cin, name);
+	
+	cout<<name;
+
+return 0;
+}
+
