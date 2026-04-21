1963535713ff2f6900211a2cf6b169b98369d3ef
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-21 21:14:20 +0500

    Added basic while loop (1 to 10 counting)
    This program prints numbers from 1 to 10 using while loop.

---
+++ 06-loops/while_basics.cpp
@@ -0,0 +1,19 @@
+// Lab: 5
+// Topic: While Loop Basics
+// Description: Prints numbers from 1 to 10
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 1;
+
+    while(num <= 10)
+    {
+        cout << num << endl;
+        num++;
+    }
+
+    return 0;
+}
