0b7b1be2cda1e15b18ad30c0843f4e1345c46300
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-22 14:34:27 +0500

    Added countdown loop using while
    Implement a countdown loop from 10 to 1 with a blast off message.

---
+++ 06-loops/countdown_loop.cpp
@@ -0,0 +1,21 @@
+// Lab: 5
+// Topic: While Loop
+// Description: Countdown from 10 to 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10;
+
+    while(num > 0)
+    {
+        cout << num << endl;
+        num--;
+    }
+
+    cout << "Blast off! 🚀";
+
+    return 0;
+}
