eec747647d08b3895f6025c11ef496ea4b637952
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-22 14:59:47 +0500

    Add multiplication table program using while loop
    Implements a program to print the multiplication table of a user-defined number using a while loop.

---
+++ 06-loops/multiplication_table.cpp
@@ -0,0 +1,22 @@
+// Lab: Loops
+// Topic: While Loop
+// Description: Prints multiplication table of a number
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, i = 1;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    while(i <= 10)
+    {
+        cout << num << " x " << i << " = " << num * i << endl;
+        i++;
+    }
+
+    return 0;
+}
