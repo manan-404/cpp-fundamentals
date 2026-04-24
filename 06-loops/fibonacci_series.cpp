4a8a54faab1feee7e3176ec6278cfef1e7cd4e52
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-04-24 15:02:56 +0500

    Added Fibonacci series program
    This program calculates and displays the Fibonacci series up to a specified number of terms.

---
+++ 06-loops/fibonacci_series.cpp
@@ -0,0 +1,22 @@
+// Description: Fibonacci series
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a = 0, b = 1, next;
+
+    cout << "Enter terms: ";
+    cin >> n;
+
+    for(int i = 1; i <= n; i++)
+    {
+        cout << a << " ";
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
