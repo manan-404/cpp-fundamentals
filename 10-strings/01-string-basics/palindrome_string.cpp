9a1e14d9046d9819ec97688c4384bb01c55c8456
Abdul Manan <abdulmananbohio.bscsf24@iba-suk.edu.pk>
2026-05-15 23:58:50 +0500

    Add string palindrome checker
    Checks whether a string reads the same forwards and backwards.

---
+++ 10-strings/01-string-basics/palindrome_string.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string to check palindrome: ";
+    getline(cin, str);
+
+    bool isPalindrome = true;
+    int n = str.length();
+    for(int i = 0; i < n/2; i++) {
+        if(str[i] != str[n-i-1]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if(isPalindrome)
+        cout << "The string is a palindrome." << endl;
+    else
+        cout << "The string is not a palindrome." << endl;
+
+    return 0;
+}
